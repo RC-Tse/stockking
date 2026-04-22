68af756e0d05dcb428c45aef6b4074370d86a773
RC-Tse <robin215248517149120@gmail.com>
2026-04-22 12:43:39 +0800

    Fix: Add missing import calculateTxParts in AnalyticsTab

--- components/AnalyticsTab.tsx
+++ components/AnalyticsTab.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useMemo, useEffect, useRef } from 'react'
-import { Holding, Transaction, UserSettings, Quote, fmtMoney, getStockName, codeOnly } from '@/types'
+import { Holding, Transaction, UserSettings, Quote, fmtMoney, getStockName, codeOnly, calculateTxParts } from '@/types'
 import { useGesture } from '@use-gesture/react'
 import { motion, useSpring, useTransform, useMotionValue } from 'framer-motion'
 import { TrendingUp, RefreshCw, Calendar as CalendarIcon, Info, Newspaper, ExternalLink } from 'lucide-react'
